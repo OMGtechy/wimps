7a6d5c9f125f482e4a047441c96ca846a2bfb5d5
Joshua Gerrard <joshuagerrard@protonmail.com>
2017-12-03 21:59:13 +0000

    Write out to a trace file

--- preload.c
+++ preload.c
@@ -25,8 +25,16 @@
 #include <execinfo.h>
 #include <stdbool.h>
 #include <stdarg.h>
+#include <fcntl.h>
+#include <linux/limits.h>
 
 #include "exit_codes.h"
+
+// should be set by wimps_setup
+int wimps_trace_fd;
+
+// should be set by glibc
+extern const char* program_invocation_short_name;
 
 bool wimps_write(int fd, const void* buffer, ssize_t size) {
     while(size > 0) {
@@ -56,8 +64,12 @@ void wimps_sigprof_handler() {
     // To get around this, we force the library to load in wimps_setup, which runs before this.
     const int addressCount = backtrace(&trace[0], size);
 
-    // TODO: write out to a file, it's meaningless on stdout...
-    wimps_write(0, trace, addressCount * elementSize);
+    // TODO: what to do if this fails?
+    //       writing an error message isn't enough...and might also fail
+    if(! wimps_write(wimps_trace_fd, trace, addressCount * elementSize)) {
+        const char* const failedWriteMessage = "WIMPS | ERR | Could not write to trace file";
+        wimps_write(STDERR_FILENO, failedWriteMessage, strlen(failedWriteMessage));
+    }
 }
 
 void wimps_force_libgcc_load() {
@@ -87,9 +99,44 @@ bool wimps_start_timer(timer_t timer) {
     return timer_settime(timer, 0, &timerSpec, NULL) == 0;
 }
 
+int wimps_create_trace_file() {
+    const int badFd = -1;
+
+    char buffer[PATH_MAX] = { '\0' };
+    snprintf(buffer, PATH_MAX, "_wimps_trace_v1_pid%d_time%.f_%s_", getpid(), difftime(time(NULL), (time_t) 0), program_invocation_short_name);
+
+    const int flags = O_WRONLY // write only access
+                    | O_APPEND // append on write
+                    | O_CREAT  // create a new file
+                    | O_EXCL;  // error if the file exists
+
+
+    const mode_t mode = S_IRUSR  // user read permissions
+                      | S_IWUSR; // user write permissions
+
+    int fd = open(buffer, flags, mode);
+
+    if(fd == badFd) {
+        // something went wrong, don't bother writing to it...
+        return badFd;
+    }
+
+    // write out a header showing that it's a wimps trace file,
+    // this is useful in case folk rename / move the trace files
+    if(! (wimps_write(fd, buffer, strlen(buffer)) && wimps_write(fd, "\n", 1))) {
+        // closing the file might change the errno, so we save and restore it
+        const int err = errno;
+        close(fd);
+        errno = err;
+        return badFd;
+    }
+
+    return fd;
+}
+
 // TODO: this should probably be refactored out of this file
 __attribute__((noreturn))
-void wimps_report_fatal_error(int exitCode, const char* const format, ...) {
+void wimps_report_fatal_error(const int exitCode, const char* const format, ...) {
     fprintf(stderr, "WIMPS | ERR | errno: %d (%s)\n", errno, strerror(errno));
 
     va_list args;
@@ -105,6 +152,11 @@ __attribute__((constructor))
 void wimps_setup() {
     // see wimps_sigprof_handler for why this is needed
     wimps_force_libgcc_load();
+
+    wimps_trace_fd = wimps_create_trace_file();
+    if(wimps_trace_fd == -1) {
+        wimps_report_fatal_error(WIMPS_CREATE_TRACE_FILE_FAILED, "WIMPS | ERR | Could not create trace file\n");
+    }
 
     if(! wimps_set_signal_handler(&wimps_sigprof_handler)) {
         wimps_report_fatal_error(WIMPS_SIGNAL_FAILED, "WIMPS | ERR | Could not set signal handler\n");
